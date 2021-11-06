3812f94fb90f759944c2a0a2cf8cc6446b3ca238
vilqo <56851676+vilqo@users.noreply.github.com>
2021-11-06 10:40:08 -0400

    Loading images from local
    Loading images from local

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
 import os
+from PIL import Image
+import glob
 
 
 def main():
-    pass
+    image_list = load_images()
+
+
+def load_images():
+    image_list = []
+    my_path = r'C:\Users\Michael Stiffler\Desktop\HackOHIO\images\*.JPG'
+    for filename in glob.glob(my_path):
+        im = Image.open(filename)
+        image_list.append(im)
+    return image_list
 
 
 if __name__ == "__main__":
